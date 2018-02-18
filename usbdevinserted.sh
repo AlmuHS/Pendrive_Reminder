e43309ae18026c0e63033ae983eab8892d84c9c6
AlmuHS <liberamenso10000@gmail.com>
2018-02-18 18:18:10 +0100

    incluida notificacion (solo en versiones modernas de polkit)

--- usbdevinserted.sh
+++ usbdevinserted.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#Copy USB identifier in usbdevinfo file.
+#This file will be used to detect if there are any usb storage device connected to the machine
 set 2>&1 | grep DEVPATH | cut -d "=" -f 2 >> /tmp/usbdevinfo
 
+
+#In polkit version < 0.106, the rules file don't run, so we need to use the old method
 if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -lt 106
 then
+	#Copy pkla file in localauthority directory
 	cp /tmp/50-inhibit-shutdown.pkla /etc/polkit-1/localauthority/50-local.d/
+
+	#Restart service
 	service polkit restart
 fi
