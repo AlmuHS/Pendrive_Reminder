73b898bcddade6dc74d7104e25c454a20b91629e
AlmuHS <liberamenso10000@gmail.com>
2018-02-18 18:31:03 +0100

    anadidos comentarios en varios scripts

--- installer.sh
+++ installer.sh
@@ -10,7 +10,9 @@ udevadm control --reload-rules
 #Copy polkit rules
 if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -ge 106
 then
+	#If polkit version is >= 0.106, copy rules file	
 	cp 10-*shutdown.rules /usr/share/polkit-1/rules.d/
 else
+	#if polkit version is < 0.106 copy pkla file to a temporal directory
 	cp 50-inhibit-shutdown.pkla /tmp
 fi
--- usbdevgone.sh
+++ usbdevgone.sh
@@ -15,6 +15,7 @@ then
 	#if file is empty, remove it
 	rm $filepath
 
+	#if polkit version is < 0.106, remove pkla file to disable polkit rule
 	if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -lt 106
 	then
 		rm /etc/polkit-1/localauthority/50-local.d/50-inhibit-shutdown.pkla
