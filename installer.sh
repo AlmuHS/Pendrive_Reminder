b7d42881ff2999fb6d96bbb58d4899f7267c5f48
AlmuHS <liberamenso10000@gmail.com>
2018-02-18 19:26:38 +0100

    anadido directorio de instalacion

--- installer.sh
+++ installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #Copy auxiliar scripts
-cp *.sh /usr/bin
+mkdir /usr/bin/pendrive-reminder
+cp *.sh /usr/bin/pendrive-reminder
 
 #copy udev rules and recharge udev
 cp 1?-usb*.rules /etc/udev/rules.d/
@@ -14,5 +15,5 @@ then
 	cp 10-*shutdown.rules /usr/share/polkit-1/rules.d/
 else
 	#if polkit version is < 0.106 copy pkla file to a temporal directory
-	cp 50-inhibit-shutdown.pkla /tmp
+	cp 50-inhibit-shutdown.pkla /usr/bin/pendrive-reminder
 fi
--- usbdevinserted.sh
+++ usbdevinserted.sh
@@ -9,7 +9,7 @@ set 2>&1 | grep DEVPATH | cut -d "=" -f 2 >> /tmp/usbdevinfo
 if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -lt 106
 then
 	#Copy pkla file in localauthority directory
-	cp /tmp/50-inhibit-shutdown.pkla /etc/polkit-1/localauthority/50-local.d/
+	cp /usr/bin/pendrive-reminder/50-inhibit-shutdown.pkla /etc/polkit-1/localauthority/50-local.d/
 
 	#Restart service
 	service polkit restart
