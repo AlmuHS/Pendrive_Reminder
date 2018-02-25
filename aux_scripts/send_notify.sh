81c26150f9c3f685bf66f077f16623b3709b6d84
AlmuHS <liberamenso10000@gmail.com>
2018-02-26 00:05:43 +0100

    eliminadas trazas y desactivada notificacion

--- aux_scripts/send_notify.sh
+++ aux_scripts/send_notify.sh
@@ -5,10 +5,7 @@ user=$1
 export DISPLAY=":0"
 export XAUTHORITY="/home/$user/.Xauthority"
 
-echo $user $XAUTHORITY > /tmp/user
-cat $XAUTHORITY > /tmp/Xauth
-
-pkexec --user $user notify-send  "Pendrive Reminder" "Shutdown lock enabled. Disconnect pendrive to enable shutdown"
+#pkexec --user $user notify-send  "Pendrive Reminder" "Shutdown lock enabled. Disconnect pendrive to enable shutdown"
 
 exit 0
 
