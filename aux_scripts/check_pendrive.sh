375ba8474d734e5265f81c9cad961804853e0906
AlmuHS <liberamenso10000@gmail.com>
2018-02-26 14:17:34 +0100

    mejorada documentacion

--- aux_scripts/check_pendrive.sh
+++ aux_scripts/check_pendrive.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#Script linked to #Script linked to 10-inhibit-shutdown polkit rule
+#This script check if watchdog file exists in the system
+#If don't exists, return 0 (correct)
+#else return 1(error)
+
+
+#Check if watchdog file exists
 if ! test -e "/tmp/usbdevinfo" 
 then
+	#don't exists -> return correct	
 	exit 0
 else
+	#exists -> return error
 	exit 1
 fi
--- aux_scripts/send_notify.sh
+++ aux_scripts/send_notify.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+#Script linked to 10-inhibit-shutdown polkit rule
+#Notifies user if polkit rule locks shutdown (there are any usb device storage in the system)
+
+#The script receives as parameter the user name, and calls notify-send command to notify user
+
+#ADVICE: THIS SCRIPT ISN'T READY TO PRODUCTION
+#The notification isn't shows in user screen 
+
+#Get username from first parameter
 user=$1
 
-export DISPLAY=":0"
-export XAUTHORITY="/home/$user/.Xauthority"
+#Creates and export display environment variables.
+#This variables will be used by notify-send to shows message in user desktop environment
 
+export DISPLAY=":0" #Display number
+export XAUTHORITY="/home/$user/.Xauthority" #Cookie file
+
+#To send notification to user, we need to use the user account who we wants to show notification 
+#To get this, we will use pkexec command
+
+#The notification is disabled because it produce lags to show shutdown menu
 #pkexec --user $user notify-send  "Pendrive Reminder" "Shutdown lock enabled. Disconnect pendrive to enable shutdown"
 
 exit 0
