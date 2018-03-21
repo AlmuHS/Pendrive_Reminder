d001b00244f370ace229c61c0287033be0c1e039
AlmuHS <liberamenso10000@gmail.com>
2018-03-22 00:10:18 +0100

    actualizada documentacion script

--- aux_scripts/send_notify.sh
+++ aux_scripts/send_notify.sh
@@ -3,13 +3,8 @@
 #Script linked to 10-inhibit-shutdown polkit rule
 #Notifies user if polkit rule locks shutdown (there are any usb device storage in the system)
 
-#The script receives as parameter the user name, and calls notify-send command to notify user
-
-#ADVICE: THIS SCRIPT ISN'T READY TO PRODUCTION
-#The notification isn't shows in user screen 
-
-#Get username from first parameter
-user=$1
+#The script use dbus-send command to send message to user using system bus and org.preminder service.
+#The user will receive message to a dbus client connected org.preminder service
 
 #send message to client using dbus system bus
 dbus-send --system /org/preminder/mensaje org.preminder.App
