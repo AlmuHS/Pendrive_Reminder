5eea8a9f21ce716b473b2ef988776dd14a0b0a8f
AlmuHS <liberamenso10000@gmail.com>
2018-03-22 13:53:39 +0100

    anadido arranque cliente dbus

--- aux_scripts/usbdevinserted.sh
+++ aux_scripts/usbdevinserted.sh
@@ -13,6 +13,11 @@
 #This file will be used to detect if there are any usb storage device connected to the machine
 set 2>&1 | grep DEVPATH | cut -d "=" -f 2 >> /tmp/usbdevinfo
 
+#Path to instalation directory
+INSTALL_DIR="/usr/bin/pendrive-reminder"
+
+#Get online users list
+user_list=$(who | cut -d " " -f 1)
 
 #In polkit version < 0.106, the rules file don't run, so we need to use the old method
 if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -lt 106
@@ -22,17 +27,21 @@ then
 	if ! test -f /etc/polkit-1/localauthority/50-local.d/50-inhibit-shutdown.pkla
 	then
 		#If it don't exists, copy pkla file in localauthority directory		
-		cp /usr/bin/pendrive-reminder/50-inhibit-shutdown.pkla /etc/polkit-1/localauthority/50-local.d/
+		cp $INSTALL_DIR/50-inhibit-shutdown.pkla /etc/polkit-1/localauthority/50-local.d/
 		service polkit restart
 	fi
+
+	#For each user, launch dbus client
+	for user in $user_list
+	do	
+		su $user -c "python $INSTALL_DIR/client.py"
+	done
+
 fi
 
 #Notify all connected users, only when first usb device is connected 
 if test $(wc -l /tmp/usbdevinfo | cut -d " " -f 1) -eq 1
 then
-	#Get online users list
-	user_list=$(who | cut -d " " -f 1)
-
 	#Send notification to all users in the list
 	for user in $user_list
 	do
