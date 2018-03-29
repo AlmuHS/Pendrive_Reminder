8c750ca63ad473e4f3db50ee86cee33c1dc9094c
AlmuHS <liberamenso10000@gmail.com>
2018-03-29 23:24:46 +0200

    cambiado Gtk por Glib en bucle principal

--- dbus-client/client.py
+++ dbus-client/client.py
@@ -3,13 +3,14 @@
 #Original code from https://stackoverflow.com/questions/22390064/use-dbus-to-just-send-a-message-in-python
 #With helpful of Alberto Caso https://es.stackoverflow.com/a/148802/26469
 
-from gi.repository import Gtk
+from gi.repository import GLib
 from gi.repository import Notify
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 
-DBusGMainLoop(set_as_default=True)
-bus = dbus.SystemBus()
+dbus_loop = DBusGMainLoop(set_as_default=True)
+bus = dbus.SystemBus(mainloop=dbus_loop)
+loop = GLib.MainLoop()
 
 def msg_handler(*args,**keywords):
     try:
@@ -21,4 +22,5 @@ def msg_handler(*args,**keywords):
         pass
 
 bus.add_signal_receiver(handler_function=msg_handler, dbus_interface='org.preminder', path_keyword='path')
-Gtk.main()
+loop.run()
+
