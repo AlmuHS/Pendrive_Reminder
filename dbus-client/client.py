967e8467bc127f0f565703f57fde20fef189f6d0
AlmuHS <liberamenso10000@gmail.com>
2018-04-13 16:22:55 +0200

    anadido soporte a locales de sistema

--- dbus-client/client.py
+++ dbus-client/client.py
@@ -9,10 +9,13 @@ import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 import gettext
 
+#Start dbus mainloop
 dbus_loop = DBusGMainLoop(set_as_default=True)
 bus = dbus.SystemBus(mainloop=dbus_loop)
 loop = GLib.MainLoop()
-linguas = gettext.translation('client', localedir='locale')
+
+#Install locale support
+linguas = gettext.translation('preminder')
 linguas.install()
 
 
@@ -21,11 +24,11 @@ def msg_handler(*args,**keywords):
         #show notification to desktop
         Notify.init('Pendrive Reminder')
         notify = Notify.Notification.new('Pendrive Reminder', _('Shutdown lock enabled. Disconnect pendrive to enable shutdown'))
-        print(_('Shutdown lock enabled. Disconnect pendrive to enable shutdown'))
         notify.show()
     except:
         pass
 
+#Wait to dbus signal
 bus.add_signal_receiver(handler_function=msg_handler, dbus_interface='org.preminder', path_keyword='path')
 loop.run()
 
